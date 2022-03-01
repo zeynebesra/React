44a56500f230d2a0fd71ec372655261115a0c047
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-03-01 11:41:38 +0300

    state end

--- states/src/App.js
+++ states/src/App.js
@@ -4,7 +4,8 @@ function App() {
   //state tanımı
   const [name, setName] = useState("Zeyneb");
   const [age, setAge] = useState(25);
-  const [friends, setFriends] = useState(["Ayşe", "Zeyneb"]);
+  const [friends, setFriends] = useState(["Ayşe", "Zeyneb"]); // array state
+  const [address, setAddress] = useState({ title: "Istanbul", zip: 34567 }); //object state
 
   console.log(age, name);
 
@@ -15,6 +16,29 @@ function App() {
 
       <button onClick={() => setName("Esra")}>Change Name</button>
       <button onClick={() => setAge(26)}>Change Age</button>
+
+      <hr />
+      <br></br>
+      <h2>Friends</h2>
+      {friends.map((friend, index) => (
+        <div key={index}>{friend}</div>
+      ))}
+      <br />
+
+      <button onClick={() => setFriends([...friends, "Büşra", "Elif"])}>
+        add new friends
+      </button>
+
+      <hr />
+      <br></br>
+      <h2>Address</h2>
+      <div>
+        {address.title} {address.zip}
+      </div>
+      <br />
+      <button onClick={() => setAddress({ ...address, title: "Ankara" })}>
+        change address
+      </button>
     </div>
   );
 }
--- states/src/index.js
+++ states/src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import InputExample from "./components/InputExample";
+import "./index.css";
+//import App from './App';
+//import Counter from "./components/Counter";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <InputExample />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
